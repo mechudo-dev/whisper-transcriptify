7c69b6da9575dccb496101ce41cedcee556f3875
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-06-27 06:04:22 -0500

    Update GoogleAdsense.tsx

--- src/components/GoogleAdsense.tsx
+++ src/components/GoogleAdsense.tsx
@@ -9,7 +9,6 @@ function AdsenseExample() {
       className='ExampleAdSlot w-28 h-60 block'
       client='ca-pub-8574483971880465'
       slot='9212563774'
-      adTest='on' //Dev Only
       data-full-width-responsive="true"
     />
   )
